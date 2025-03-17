d20c9eda3eaa6bc63b21a873c5ada4af964a3137
gustavozibini <gustavo_belizario@hotmail.com>
2025-03-17 20:52:16 -0300

    Adicionado variaveis e calculo de media.

--- primeira_aula_pyton.py
+++ primeira_aula_pyton.py
@@ -1,1 +1,10 @@
-print("hello world!!")
+#print("hello world!!")
+'''print("hello world!!")'''
+check1 = 9
+check2 = 7.5
+check3 = 8
+
+media = (check1 + check2 + check3)/3
+print(media)
+#print((check1 + check2 + check3)/3)
+print(f"Média = {media:.2f}")
