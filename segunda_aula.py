09bc9ba5730294417af2b29b1091dbfcda6123ed
gustavozibini <gustavo_belizario@hotmail.com>
2025-03-29 14:09:54 -0300

    teste

--- segunda_aula.py
+++ segunda_aula.py
@@ -1,1 +1,18 @@
 # fazer os 3 primeiros exercicios
+
+# ex1
+# print("meu nome é gustavo")
+
+# #ex2
+# a = 3
+# b = 5
+# resultado = (2 * a) * (3 * b)
+#
+# print(f"O resultado é: {resultado}")
+
+# #ex3
+# a = 1
+# b = 2
+# c = 3
+# soma = a + b + c
+# print(f"a soma das variaveis é de {soma}")
--- terceira_aula.py
+++ terceira_aula.py
@@ -10,3 +10,89 @@
 # bonus = anos * valor_por_ano
 # print("Bonus de R$ ", bonus)
 
+# ex:4
+# num1 =int(input("digite o primero numero"))
+# num2 =int(input("digite o segundo numero"))
+# soma = num1 + num2
+#
+# print(f"a soma dos números é {soma}")
+
+# # ex5
+# metros = float(input("digite a metragem: "))
+# milimetros = metros * 1000000
+#
+# print(f"{metros} metros equivale a {milimetros} milimetros.")
+
+# ex6
+# dia = int(input("digite a quantidade de dias: "))
+# hora = int(input("digite a quantidade de horas: "))
+# minuto = int(input("digite a quantidade de minutos: "))
+# segundo = int(input("digite a quantidade de segundos: "))
+#
+# convert_dia = dia * 24 * 60 * 60
+# convert_hora = hora * 60 * 60
+# convert_minuto = minuto * 60
+#
+# total_segundo = convert_dia + convert_hora + convert_minuto + segundo
+#
+# print(f"o total de segundos é de: {total_segundo}")
+
+# # ex7
+# salario = float(input("digite o salario: "))
+# porcentagem_aumento = float(input("digite a porcentagem do aumento: "))
+#
+# aumento = (porcentagem_aumento / 100) * salario
+# novo_salario = salario + aumento
+#
+# print(f"o valor do salario aumentou para: {aumento}")
+# print(f"o novo salario passa a ser de: {novo_salario}")
+
+# # ex8
+# preco = float(input("digite o valor da mercadoria: "))
+# desconto = float(input("digite o valor do desconto: "))
+#
+# desconto = desconto / 100 * preco
+# novo_preco = preco - desconto
+#
+# print(f" o valor da mercadoria pasou a ser de: {novo_preco}")
+# print(f" o valor do desconto é de: {desconto}")
+
+# #ex9
+# distancia = int(input("digite a distância em km: "))
+# velocidade_media = int(input("digite a velocidade média em km/h: "))
+#
+# tempo = distancia / velocidade_media
+#
+# hora = int(tempo)
+# minuto = int ((tempo - hora) * 60)
+#
+# print(f"O tempo estimado da viagem é de: {hora} horas e {minuto} minutos")
+
+# # ex10
+# temperatura = float(input("Digite a temperatura em celsius: "))
+#
+# converter = ((9 * temperatura) / 5) + 32
+#
+# print(f"a temperatura convertida é de: {converter}fahrenheit")
+
+# #ex11
+# km = int(input("digite a distancia percorrida: "))
+# dia = int(input("digite a quantidade de dias: "))
+#
+# preco = (60 * dia) + (0.15 * km)
+#
+# print(f"o valor a pagar é de R${preco:.2f}")
+
+# # ex12
+# x = int(input("Digite o valor de x "))
+# y = int(input("Digite o valor de y "))
+#
+# z = ((x**2) + (y**2)) / ((x-y)**2)
+#
+# print(z)
+
+# ex13
+# salario = float(input("digite o salario: "))
+# reajuste = (0.35 * salario + salario)
+#
+# print(f"O salario com reajuste de 35% é de R${reajuste:.2f}")
