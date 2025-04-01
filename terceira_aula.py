2b93269bfc7c69bfbe2dbddaffd870395ef62b46
gustavozibini <gustavo_belizario@hotmail.com>
2025-03-31 21:03:23 -0300

    exercicios aula 3 if else

--- terceira_aula.py
+++ terceira_aula.py
@@ -10,20 +10,20 @@
 # bonus = anos * valor_por_ano
 # print("Bonus de R$ ", bonus)
 
-# ex:4
+# ex:4------------------------------------------------------------------------
 # num1 =int(input("digite o primero numero"))
 # num2 =int(input("digite o segundo numero"))
 # soma = num1 + num2
 #
 # print(f"a soma dos números é {soma}")
 
-# # ex5
+# # ex5------------------------------------------------------------------------
 # metros = float(input("digite a metragem: "))
 # milimetros = metros * 1000000
 #
 # print(f"{metros} metros equivale a {milimetros} milimetros.")
 
-# ex6
+# ex6------------------------------------------------------------------------
 # dia = int(input("digite a quantidade de dias: "))
 # hora = int(input("digite a quantidade de horas: "))
 # minuto = int(input("digite a quantidade de minutos: "))
@@ -37,7 +37,7 @@
 #
 # print(f"o total de segundos é de: {total_segundo}")
 
-# # ex7
+# # ex7------------------------------------------------------------------------
 # salario = float(input("digite o salario: "))
 # porcentagem_aumento = float(input("digite a porcentagem do aumento: "))
 #
@@ -47,17 +47,17 @@
 # print(f"o valor do salario aumentou para: {aumento}")
 # print(f"o novo salario passa a ser de: {novo_salario}")
 
-# # ex8
+# # ex8------------------------------------------------------------------------
 # preco = float(input("digite o valor da mercadoria: "))
 # desconto = float(input("digite o valor do desconto: "))
 #
 # desconto = desconto / 100 * preco
 # novo_preco = preco - desconto
 #
-# print(f" o valor da mercadoria pasou a ser de: {novo_preco}")
+# print(f" o valor da mercadoria passou a ser de: {novo_preco}")
 # print(f" o valor do desconto é de: {desconto}")
-
-# #ex9
+#
+# #ex9------------------------------------------------------------------------
 # distancia = int(input("digite a distância em km: "))
 # velocidade_media = int(input("digite a velocidade média em km/h: "))
 #
@@ -67,15 +67,16 @@
 # minuto = int ((tempo - hora) * 60)
 #
 # print(f"O tempo estimado da viagem é de: {hora} horas e {minuto} minutos")
-
-# # ex10
+#
+# # ex10-----------------------------------------------------------------------
+#
 # temperatura = float(input("Digite a temperatura em celsius: "))
 #
 # converter = ((9 * temperatura) / 5) + 32
 #
-# print(f"a temperatura convertida é de: {converter}fahrenheit")
+# print(f"a temperatura convertida é de: {converter} fahrenheit")
 
-# #ex11
+# #ex11------------------------------------------------------------------------
 # km = int(input("digite a distancia percorrida: "))
 # dia = int(input("digite a quantidade de dias: "))
 #
@@ -83,7 +84,7 @@
 #
 # print(f"o valor a pagar é de R${preco:.2f}")
 
-# # ex12
+# # ex12------------------------------------------------------------------------
 # x = int(input("Digite o valor de x "))
 # y = int(input("Digite o valor de y "))
 #
@@ -91,7 +92,7 @@
 #
 # print(z)
 
-# ex13
+# ex13------------------------------------------------------------------------
 # salario = float(input("digite o salario: "))
 # reajuste = (0.35 * salario + salario)
 #
---
+++ terceira_aula_if_else.py
@@ -0,0 +1,50 @@
+# media_final = float(input("Digite a média de 0 a 10: "))
+#
+# if media_final >=6:
+#     print("aprovado!")
+# else:
+#     print("reprovado!")
+#
+# ex1:
+# velocidade = int(input("Digite a velocidade do carro: "))
+#
+# if velocidade > 80:
+#     multa = (velocidade - 80) * 5
+#     print (f"Você ultrapassou o limite de velocidade e foi multado em: R${multa:.2f}")
+# else:
+#     print("Você não foi multado")
+#
+#
+#
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: "))
+# num3 = float(input("Digite o terceiro número: "))
+#
+# # if num1 >= num2 and num1 >= num3:
+# #     print(f"O maior numero é {num1}")
+# # elif num2 >= num1 and num2 >= num3:
+# #     print(f"O maior numero é {num2}")
+# # elif num3 >= num1 and num3 >= num2:
+# #     print(f"O maior numero é {num3}")
+# #
+# # if num1 <= num2 and num1 <= num3:
+# #     print(f"O menor numero é {num1}")
+# # elif num2 <= num1 and num2 <= num3:
+# #     print(f"O menor numero é {num2}")
+# # elif num3 <= num1 and num3 <= num2:
+# #     print(f"O menor numero é {num3}")
+#
+# maior = num1
+# if num2 >= num1 and num2 >= num3:
+#     maior = num2
+# if num3 >= num1 and num3 >= num2:
+#     maior = num3
+#
+# menor = num1
+# if num2 >= num1 and num2 >= num3:
+#     maior = num2
+# if num3 >= num1 and num3 >= num2:
+#     maior = num3
+#
+# print(maior)
+# print(menor)
