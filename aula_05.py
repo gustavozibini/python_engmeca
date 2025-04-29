a673679b4c6265a568e4d73d06e76ceaca338a67
gustavozibini <gustavo_belizario@hotmail.com>
2025-04-28 21:01:37 -0300

    exercicios if else

---
+++ aula_05.py
@@ -0,0 +1,27 @@
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# op = input("Digite a operacao: ")
+#
+# if op == "+":
+#     soma = n1 + n2
+# elif op == "-":
+#     soma = n1 - n2
+# elif op == "*":
+#     soma = n1 * n2
+# elif op == "/":
+#     soma = n1 / n2
+# else:
+#     print("operacao invalida!")
+# print(f"Resultado: {soma}")
+#
+# fim = int(input("digite o number"))
+# x = 1
+# while x <= fim:
+#     print(x)
+#     x += 2
+#
+# fim = int(input("digite o number"))
+# x = 3
+# while x <= 30:
+#     print(x)
+#     x += 3
